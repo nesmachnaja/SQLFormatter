c21a220eee145bdfeffec5d60f3da81871cb89e4
Людмила Несмачная <lnesmachnaya@octopeople.net>
2024-10-17 11:07:44 +0300

    detectEncoding and extra regexps for comments

--- SQLFormatter/Program.cs
+++ SQLFormatter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace SQLFormatter
@@ -13,21 +14,55 @@ namespace SQLFormatter
             string[] sqlFiles = Directory.GetFiles(folderPath, "*.sql");
 
             foreach (string filePath in sqlFiles)
-                FormatFile(filePath);
+            {
+                Console.WriteLine($"Detected encoding: {DetectEncoding(filePath)}");
 
+                //Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                //Encoding windows1251 = Encoding.GetEncoding(1251);
+                //string fileContent = File.ReadAllText(filePath);
+
+                FormatFile(filePath);
+            }
+            //using (StreamReader reader = new StreamReader(sqlFiles[0], true)) // true - для автоопределения кодировки
+            //{
+            //    string fileContent = reader.ReadToEnd();
+            //    System.Console.WriteLine("Содержимое файла:");
+            //    System.Console.WriteLine(fileContent);
+            //    System.Console.WriteLine("Кодировка: " + reader.CurrentEncoding);
+            //}
+        }
+
+        static string DetectEncoding(string filePath)
+        {
+            byte[] fileBytes = File.ReadAllBytes(filePath);
+            string detectedEncoding = "Unknown or ANSI";
+
+            if (fileBytes.Length >= 3 && fileBytes[0] == 0xEF && fileBytes[1] == 0xBB && fileBytes[2] == 0xBF)
+            {
+                detectedEncoding = "UTF-8 with BOM";
+            }
+            else if (fileBytes.Length >= 2 && fileBytes[0] == 0xFF && fileBytes[1] == 0xFE)
+            {
+                detectedEncoding = "UTF-16 LE";
+            }
+            else if (fileBytes.Length >= 2 && fileBytes[0] == 0xFE && fileBytes[1] == 0xFF)
+            {
+                detectedEncoding = "UTF-16 BE";
+            }
+            return detectedEncoding;
         }
 
         private static void FormatFile(string filePath)
         {
             try
             {
-                string sqlContent = File.ReadAllText(filePath);
+                string sqlContent = File.ReadAllText(filePath, Encoding.UTF8);
 
-                int createProcedureIndex = Regex.Match(sqlContent, @"\bcreate\b\s+\bprocedure\b", RegexOptions.IgnoreCase).Index;
+                int createProcedureIndex = Regex.Match(sqlContent, @"\bcreate\b\s+\bprocedure\b|\bcreate\b\s+\bview\b", RegexOptions.IgnoreCase).Index;
 
                 if (createProcedureIndex == -1)
                 {
-                    Console.WriteLine("CREATE PROCEDURE не найдено в файле.");
+                    Console.WriteLine("CREATE PROCEDURE или CREATE VIEW не найдено в файле.");
                     return;
                 }
 
@@ -39,10 +74,12 @@ namespace SQLFormatter
                 beforeCreateProcedure = Regex.Replace(beforeCreateProcedure, @"--\s*Modified\s+by\s*:", $"-- Created by       :", RegexOptions.IgnoreCase);
 
                 // Удаление всех однострочных комментариев (начинаются с "--")
-                afterCreateProcedure = Regex.Replace(afterCreateProcedure, @"^[\t\s]*--(?!.*\bStep\b).*?(?:\r?\n|$)", "\n", RegexOptions.Multiline);
+                afterCreateProcedure = Regex.Replace(afterCreateProcedure, @"^[\t\s]*--(?!.*\bStep\b).*?(?:\r?\n|$)", "\r\n", RegexOptions.Multiline);
+                afterCreateProcedure = Regex.Replace(afterCreateProcedure, @"(\r?\n){4,}", "\r\n\r\n");
 
                 // Удаление всех многострочных комментариев (начинаются с "/*" и заканчиваются "*/")
                 afterCreateProcedure = Regex.Replace(afterCreateProcedure, @"/\*.*?\*/", "", RegexOptions.Singleline);
+                afterCreateProcedure = Regex.Replace(afterCreateProcedure, @" {2,}", " ");
 
                 // Замена команд SQL на верхний регистр в части после "create procedure"
                 afterCreateProcedure = Regex.Replace(afterCreateProcedure, @"\bcreate\b", "CREATE", RegexOptions.IgnoreCase);
@@ -89,7 +126,7 @@ namespace SQLFormatter
 
                 string updatedContent = beforeCreateProcedure + afterCreateProcedure;
 
-                File.WriteAllText(filePath, updatedContent);
+                File.WriteAllText(filePath, updatedContent, Encoding.Unicode);
 
                 Console.WriteLine("SQL команды успешно заменены на верхний регистр, комментарии удалены.");
             }
